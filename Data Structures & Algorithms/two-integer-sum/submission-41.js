a31cf3bf7fae2bacc5f95052be89999165f30288
Kadir Aslan <kdr.aslan@yahoo.com>
2026-07-03 20:56:45 +0200

    Add: two-integer-sum - submission-41

---
+++ Data Structures & Algorithms/two-integer-sum/submission-41.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let lookup = {};
+
+        try {
+            nums.forEach((num, index) => {
+                if (lookup[num]) {
+                    lookup[num] = [...lookup[num], index];
+                } else {
+                    lookup[num] = [index];
+                }
+
+                if (lookup[target-num]) {
+                    if (!(target-num == num && lookup[num].length == 1)) {
+                        throw [index, lookup[target-num][0]];
+                    }
+                }
+            });
+        } catch (e) {
+            console.log(e);
+
+            return e;
+        }
+    }
+}
