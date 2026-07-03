65dc9046a0539dc788a2107ac548eb67c2c0af76
Kadir Aslan <kdr.aslan@yahoo.com>
2026-07-03 16:28:24 +0200

    Add: is-anagram - submission-17

---
+++ Data Structures & Algorithms/is-anagram/submission-17.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        let chars = {}
+
+        if (s.length !== t.length) {
+            return false;
+        }
+
+        s.split('').forEach(letter => {
+            if (chars[letter]) {
+                chars[letter] = chars[letter] + 1;
+            } else {
+                chars[letter] = 1;
+            }
+        });
+
+        console.log(chars);
+
+        try {
+            t.split('').forEach(letter => {
+                if (chars[letter]) { 
+                    if (chars[letter] > 1) {
+                    chars[letter] = chars[letter] - 1;
+                    } else if (chars[letter] == 1) {
+                        delete chars[letter]
+                    } 
+                }
+                else {
+                    throw Exception;
+                }
+            });
+        } catch {
+            return false;
+        }
+
+        return !Boolean(Object.keys(chars).length);
+    }
+}
