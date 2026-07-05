f2caa14baf1bc2c2c6a1539387ee9e03e6bb9415
Kadir Aslan <kdr.aslan@yahoo.com>
2026-07-05 21:58:49 +0200

    Add: products-of-array-discluding-self - submission-21

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-21.js
@@ -0,0 +1,55 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let output = [];
+        let leftRunningSum = [];
+        let rightRunningSum = [];
+
+        let zeroCount = 0;
+        let zeroIndex = -1;
+        let totalFactor = 1;
+
+        nums.forEach((num, index) => {
+            output.push(0);
+            leftRunningSum.push(1);
+            rightRunningSum.push(1);
+
+            if (num === 0) {
+                zeroCount += 1; 
+                zeroIndex = index;
+            } else {
+                totalFactor *= num;
+            }
+        });
+
+        if (zeroCount === 1) {
+            output[zeroIndex] = totalFactor; 
+
+            return output;
+        } else if (zeroCount > 1) {
+            return output;
+        } else {
+            for(let i = 1; i < nums.length; i++) {
+                leftRunningSum[i] = nums[i-1] * leftRunningSum[i-1];
+            }
+
+            console.log(leftRunningSum);
+[1, 1, 2, 8]
+[48, 24, 6, 1]
+            for(let i = nums.length - 2; i >= 0; i--) {
+                rightRunningSum[i] = nums[i+1] * rightRunningSum[i + 1];
+            }
+
+            console.log(rightRunningSum);
+
+            for (let i = 0; i < nums.length; i++) {
+                output[i] = leftRunningSum[i]*rightRunningSum[i];
+            }
+
+            return output;
+        }
+    }
+}
